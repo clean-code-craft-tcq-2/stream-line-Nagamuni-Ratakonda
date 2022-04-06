a2c2b8cf936448b03a0855b0dc8e14f447f622ba
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-06 20:49:36 +0530

    Update test_BMS_DataSender.cpp

--- test_BMS_DataSender.cpp
+++ test_BMS_DataSender.cpp
@@ -4,6 +4,30 @@
 #include "BMS_DataSender.h"
 
 TEST_CASE("Testcase to check whether Sensor data is read successfully from a file") {
-  float Temperature[10], SOC[10], ChargeRate[10];
+  float Temperature[TotalNoOfReadings], SOC[TotalNoOfReadings], ChargeRate[TotalNoOfReadings];
+  float test_Temperature[TotalNoOfReadings], test_SOC[TotalNoOfReadings], test_ChargeRate[TotalNoOfReadings];
+  float temp,soc,chrRate;
+  int index;
+  
+  // Verify data is fetched from sender
   REQUIRE(GetBMSDataFromSender(&Temperature[0],&SOC[0],&ChargeRate[0]) == true);
+  
+  FILE *test_SensorData_fp = fopen("./BMSSensorData.txt","r");
+  
+  for(index = 0; fscanf(BMSSensorDataFile_fp, "%f %f %f\n", &temp, &soc, &chrRate)!=EOF; index++)
+  {
+      test_Temperature[index] = temp;
+      test_SOC[index] = soc;
+      test_ChargeRate[index] = chrRate;
+  }
+  
+  //Verify that readings are read from input file
+  for(index = 0; index < TotalNoOfReadings; index++)
+  {
+    REQUIRE(Temperature[i] == test_Temperature[i]);
+    REQUIRE(SOC[i] == test_SOC[i]);
+    REQUIRE(ChargeRate[i] == test_ChargeRate[i]);
+  }
+  
+  fclose(test_SensorData_fp);
 }
