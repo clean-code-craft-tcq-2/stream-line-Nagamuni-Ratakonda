5994f04c07c6e33052c6f67e519f590607a91347
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-06 21:35:41 +0530

    Update BMS_DataSender.h

--- BMS_DataSender.h
+++ BMS_DataSender.h
@@ -4,6 +4,6 @@
 #define TotalNoOfReadings 10
 
 
-bool GetBMSDataFromSender(float *Temperature, float *SOC, float *ChargeRate, string inputFile);
+bool GetBMSDataFromSender(float *Temperature, float *SOC, float *ChargeRate, char inputFile);
 void TransferBMSSensorToConsole(float *Temperature, float *SOC, float *ChargeRate);
 bool BMSSender(float *Temperature, float *SOC, float *ChargeRate, string inputFile);
