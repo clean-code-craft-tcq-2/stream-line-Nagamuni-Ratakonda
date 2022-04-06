c095373fab0ab0692e85a5b3426795767014719c
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-06 20:10:56 +0530

    Update test_BMS_DataSender.cpp

--- test_BMS_DataSender.cpp
+++ test_BMS_DataSender.cpp
@@ -1,3 +1,8 @@
 #define CATCH_CONFIG_MAIN 
 
 #include "test/catch.hpp"
+
+TEST_CASE("Testcase to check whether Sensor data is read successfully from a file") {
+  float Temperature[10], SOC[10], ChargeRate[10];
+  REQUIRE(GetBMSDataFromSender(&Temperature[0],&SOC[0],&ChargeRate[0]) == true);
+}
