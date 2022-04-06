e588ccf1c465a4afed547c4fc9a39b2eea681959
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-06 19:40:54 +0530

    Update BMS_DataSender.c

--- BMS_DataSender.c
+++ BMS_DataSender.c
@@ -1,1 +1,30 @@
 #include "BMS_DataSender.h"
+
+bool GetBMSDataFromSender(float *Temperature, float *SOC, float *ChargeRate)
+{
+  bool isDataReadFromSender = false;
+  float currentTemperature, currentSOC, currentChargeRate;
+  int Readingindex = 0;
+  
+  FILE *BMSSensorDataFile_fp = fopen("./BMSSensorData.txt","r");
+  
+  if(NULL_PTR != BMSSensorDataFile_fp)
+  {
+    while(EOF != fscanf(BMSSensorDataFile_fp, "%f, %f, %f\n", &currentTemperature, &currentSOC, &currentChargeRate))
+    {
+      Temperature[Readingindex] = currentTemperature;
+      SOC[Readingindex] = currentSOC;
+      ChargeRate[Readingindex] = currentChargeRate;
+      Readingindex++;
+    }
+    isDataReadFromSender = true;
+  }
+  else
+  {
+    printf("Error in opening the File BMSSensorData.txt");
+  }
+  
+  fclose(BMSSensorDataFile_fp);
+  
+  return isDataReadFromSender;
+}
