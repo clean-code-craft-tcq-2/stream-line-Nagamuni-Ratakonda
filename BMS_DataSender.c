6d9623bf203b14e9d75fa1377f2300eb400fe008
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-06 20:38:38 +0530

    Update BMS_DataSender.c

--- BMS_DataSender.c
+++ BMS_DataSender.c
@@ -11,7 +11,7 @@ bool GetBMSDataFromSender(float *Temperature, float *SOC, float *ChargeRate)
   if(NULL != BMSSensorDataFile_fp)
   {
     printf("File opened successfully\n");
-    for(Readingindex = 0; fscanf(BMSSensorDataFile_fp, "%f,%f,%f\n", &currentTemperature, &currentSOC, &currentChargeRate)!=EOF; Readingindex++)
+    for(Readingindex = 0; fscanf(BMSSensorDataFile_fp, "%f %f %f\n", &currentTemperature, &currentSOC, &currentChargeRate)!=EOF; Readingindex++)
     {
       Temperature[Readingindex] = currentTemperature;
       SOC[Readingindex] = currentSOC;
