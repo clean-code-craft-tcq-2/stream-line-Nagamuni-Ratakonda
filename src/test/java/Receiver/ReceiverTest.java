f9f033ddbb827be079520744327cf9ce482eea44
Swetha K A <k.a.swetha850@gmail.com>
2022-04-22 12:12:52 +0530

    Update ReceiverTest

--- src/test/java/Receiver/ReceiverTest.java
+++ src/test/java/Receiver/ReceiverTest.java
@@ -1,8 +1,5 @@
 package Receiver;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,21 +9,6 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ReceiverTest {
-
-  private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-  @Before
-  public void setUp() {
-    System.setOut(new PrintStream(this.outputStreamCaptor));
-  }
-
-  @Test
-  public void testPrintToConsole() throws IOException {
-    Receiver.printToConsole(Receiver.TEMP, 1.0f, 19.0f, 15.0f);
-
-    Assert.assertEquals("TEMPERATURE\r\n" + "Minimum TEMPERATURE: 1.0\r\n" + "Maximum TEMPERATURE: 19.0\r\n" + "Moving Average for TEMPERATURE: 15.0", this.outputStreamCaptor.toString().trim());
-  }
-
   @Test
   public void testGetLastFiveValues() {
     List<Float> list = Arrays.asList(0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f);
@@ -55,8 +37,12 @@ public class ReceiverTest {
     Assert.assertEquals(1.0f, max, 0.0f);
   }
 
-  @After
-  public void tearDown() {
-    System.setOut(System.out);
+  @Test
+  public void testGetValuesfromConsoleLine() {
+    String line = "1.000000    2.000000    0.100000";
+    String[] values = Receiver.getValuesfromConsoleLine(line);
+    Assert.assertEquals("1.000000", values[0]);
+    Assert.assertEquals("2.000000", values[1]);
+    Assert.assertEquals("0.100000", values[2]);
   }
 }
